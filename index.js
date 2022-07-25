0cdc7552e769a84bbd4271e298717482c4a5b23e
phvleite <phvleite@gmail.com>
2022-07-25 15:40:38 -0300

    [REQ-01] realizado

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv/config');
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,7 +17,16 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const upsideDown = process.env.UPSIDEDOWN_MODE;
+
+let hereIsTheUpsideDown;
+if (upsideDown === 'true') {
+  hereIsTheUpsideDown = true;
+} else {
+  hereIsTheUpsideDown = false;
+}
+
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +37,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Escutando na porta 3000');
 });
